581dd354b7af51fab35eb88dec490fbc6aba764e
Sebastian Ballen <sebastian.ballenc@gmail.com>
2022-01-22 16:42:58 -0500

    ESLint Airbnb config added to projects config.

---
+++ .eslintrc.js
@@ -0,0 +1,38 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: ["plugin:react/recommended", "airbnb", "prettier"],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 12,
+    sourceType: "module",
+  },
+  plugins: ["react"],
+  rules: {
+    "prefer-promise-reject-errors": ["off"],
+    "react/jsx-filename-extension": ["off"],
+    "react/prop-types": ["off"],
+    "no-return-assign": ["off"],
+    "global-require": ["off"],
+    "no-multi-assign": ["warn"],
+    "no-empty": ["warn"],
+    "react/no-danger": ["off"],
+    "no-shadow": ["off"],
+    "react/forbid-prop-types": ["off"],
+    "react/jsx-props-no-spreading": ["off"],
+    "no-undef": ["off"],
+    "import/prefer-default-export": ["off"],
+    "import/no-unresolved": ["off"],
+    "import/no-cycle": ["off"],
+    "react/no-array-index-key": ["off"],
+    "no-param-reassign": ["error", { props: false }],
+    "no-case-declarations": ["off"],
+    "jsx-a11y/label-has-associated-control": ["off"],
+    "jsx-a11y/click-events-have-key-events": ["off"],
+    "jsx-a11y/no-static-element-interactions": ["off"],
+  },
+};
