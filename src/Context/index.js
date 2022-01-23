28890064ca66c76cd6237a824b9a1d160fdb2554
Sebastian Ballen <sebastian.ballenc@gmail.com>
2022-01-23 12:00:01 -0500

    Functions, props & data added to context.

--- src/Context/index.js
+++ src/Context/index.js
@@ -1,11 +1,33 @@
+/* eslint-disable react/jsx-no-constructed-context-values */
 import React from 'react';
 
 const Context = React.useContext();
 
 function Provider(props) {
+  const [people, setPeople] = React.useState([]);
+  const [loading, setLoading] = React.useState(true);
+  const [openModal, setOpenModal] = React.useState(false);
   const { children } = props;
+
+  React.useEffect(() => {
+    async function fetchPeople() {
+      const response = await fetch('https://swapi.dev/api/people');
+      const data = await response.json();
+      setPeople(data.results)
+      setLoading(false)
+    }
+    fetchPeople()
+  }, []);
+
   return (
-    <Context.Provider>
+    <Context.Provider value={{
+      people,
+      setPeople,
+      loading,
+      setLoading,
+      openModal,
+      setOpenModal
+    }}>
       {children}
     </Context.Provider>)
 };
