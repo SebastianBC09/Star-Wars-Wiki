31a9785e97c6f8922c6abbbad9e0fb602c8c92c7
Sebastian Ballen <sebastian.ballenc@gmail.com>
2022-01-22 20:00:04 -0500

    Testing API & fetching data. It's successfully working.

--- src/App.js
+++ src/App.js
@@ -1,8 +1,21 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { Navbar } from './Navbar';
 import './App.scss';
 
 function App() {
+  const [people, setPeople] = React.useState([]);
+  const [loading, setLoading] = React.useState(false);
+
+  useEffect(() => {
+    async function fetchPeople() {
+      let response = await fetch('https://swapi.dev/api/people');
+      let data = await response.json();
+      setPeople(data.results)
+    }
+
+    fetchPeople()
+  }, []);
+  console.log('data', people);
   return (
     <section className="App">
       <Navbar />
