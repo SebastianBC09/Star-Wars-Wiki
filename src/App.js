e4d5defb849fe1365fe9ce682c64aeab7725a129
Sebastian Ballen <sebastian.ballenc@gmail.com>
2022-01-22 23:59:10 -0500

    CharacterList component finished. Character card component added to component & MUI used to center items.

--- src/App.js
+++ src/App.js
@@ -1,24 +1,37 @@
 import React, { useEffect } from 'react'
+import Stack from '@mui/material/Stack';
+import CircularProgress from '@mui/material/CircularProgress';
 import { Navbar } from './Navbar';
+import { CharacterCard } from './CharacterCard';
 import './App.scss';
+import { CharacterList } from './CharacterList';
 
 function App() {
   const [people, setPeople] = React.useState([]);
-  const [loading, setLoading] = React.useState(false);
+  const [loading, setLoading] = React.useState(true);
 
   useEffect(() => {
     async function fetchPeople() {
       let response = await fetch('https://swapi.dev/api/people');
       let data = await response.json();
       setPeople(data.results)
+      setLoading(false)
     }
-
     fetchPeople()
   }, []);
-  console.log('data', people);
   return (
     <section className="App">
       <Navbar />
+      {loading ? (
+        <Stack sx={{ color: 'grey.500' }} spacing={2} direction="row">
+          <CircularProgress color="secondary" />
+          <CircularProgress color="success" />
+          <CircularProgress color="inherit" />
+        </Stack>
+      ) : (
+        // <CharacterCard data={people} />
+        <CharacterList data={people} />
+      )}
     </section>
   );
 }
--- src/CharacterList.js
+++ src/CharacterList.js
@@ -1,7 +1,21 @@
 import React from 'react';
+import Box from '@mui/material/Box';
+import Container from '@mui/material/Container';
+import Typography from '@mui/material/Typography';
+import { CharacterCard } from './CharacterCard';
 
-function CharacterList() {
-  return ();
+function CharacterList(props) {
+  const { data } = props;
+  return (
+    <Container maxWidth="xl">
+      <Box >
+        <Typography variant="h2" mt={1.875} mb={1.875} color="warning.light">
+          Characters
+        </Typography>
+        <CharacterCard data={data} />
+      </Box>
+    </Container >
+  );
 }
 
 export { CharacterList };
