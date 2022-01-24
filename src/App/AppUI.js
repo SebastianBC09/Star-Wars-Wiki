9c5b0867ec3e02e469b8071dae74e46dc63d452d
Sebastian Ballen <sebastian.ballenc@gmail.com>
2022-01-23 21:04:02 -0500

    Redundancy of components deleted.

--- src/App/AppUI.js
+++ src/App/AppUI.js
@@ -2,7 +2,6 @@ import React from 'react';
 import { Context } from '../Context'
 import { Navbar } from '../Navbar';
 import { ViewCharacterList } from '../ViewCharacterList'
-import { CharacterList } from '../CharacterList';
 
 function AppUI() {
   const { loading } = React.useContext(Context);
@@ -11,9 +10,7 @@ function AppUI() {
     <>
       <Navbar />
       {loading && <p>Estamos cargando, no desesperes...</p>}
-      <ViewCharacterList>
-        <CharacterList />
-      </ViewCharacterList>
+      <ViewCharacterList />
     </>
   );
 };
