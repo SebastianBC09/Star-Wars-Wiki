29c1f4d89b6ad466a434a570172fc2bc8a295d62
Sebastian Ballen <sebastian.ballenc@gmail.com>
2022-01-23 20:59:29 -0500

    More props added to characters card.

--- src/CharacterList/index.js
+++ src/CharacterList/index.js
@@ -1,35 +1,40 @@
 import React from 'react';
 import Grid from '@mui/material/Grid';
 import Card from '@mui/material/Card';
-import CardActions from '@mui/material/CardActions';
 import CardContent from '@mui/material/CardContent';
-import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
+import { Context } from '../Context'
 
-function CharacterList(props) {
-  const { data } = props;
+function CharacterList() {
+  const { people } = React.useContext(Context)
   return (
     <Grid container rowSpacing={2} columnSpacing={{ xs: 1, sm: 2, md: 4 }}>
-      {data.map((people, i) => (
-        <Grid item lg={4}>
+      {people.map((people, index) => (
+        <Grid item sm={5} md={4} lg={4} xl={2.4} key={index}>
           <Card variant="outlined" sx={{ minWidth: 250, minHeight: 100, bgcolor: 'grey.900' }}>
-            <CardContent key={i}>
-              <Typography sx={{ fontSize: 16, color: 'white' }} color="text.secondary" gutterBottom>
-                {people.name}
+            <CardContent>
+              <Typography sx={{ fontSize: 18, color: 'white' }} color="text.secondary" align='center' gutterBottom>
+                Character Name: {people.name}
               </Typography>
-              <Typography variant="body2" sx={{ color: 'error.dark' }}>
-                {people.birth_year}
+              <Typography variant="body1" sx={{ color: 'error.dark' }}>
+                Birth Year: {people.birth_year}
+              </Typography>
+              <Typography variant="body1" sx={{ color: 'error.dark' }}>
+                Height: {people.height}
+              </Typography>
+              <Typography variant="body1" sx={{ color: 'error.dark' }}>
+                Skin Color: {people.skin_color}
+              </Typography>
+              <Typography variant="body1" sx={{ color: 'error.dark' }}>
+                Eye Color: {people.eye_color}
+              </Typography>
+              <Typography variant="body1" sx={{ color: 'error.dark' }}>
+                Gender: {people.gender}
               </Typography>
             </CardContent>
-            <CardActions>
-              <Button size="medium" variant='text' sx={{ bgcolor: 'warning.dark', color: 'white' }}>
-                More Info
-              </Button>
-            </CardActions>
           </Card>
         </Grid>
-      ))
-      }
+      ))}
     </Grid >
   )
 }
