3d50bd4ae430f65b8676bf30c23d25b17eecb68c
Sebastian Ballen <sebastian.ballenc@gmail.com>
2022-01-23 20:57:46 -0500

    Loader added to View Character List.

--- src/App/AppUI.js
+++ src/App/AppUI.js
@@ -1,32 +1,19 @@
 import React from 'react';
-import Stack from '@mui/material/Stack';
-import CircularProgress from '@mui/material/CircularProgress';
 import { Context } from '../Context'
 import { Navbar } from '../Navbar';
 import { ViewCharacterList } from '../ViewCharacterList'
 import { CharacterList } from '../CharacterList';
 
 function AppUI() {
-  const { people, loading, openModal, setOpenModal } = React.useContext(Context);
+  const { loading } = React.useContext(Context);
 
   return (
     <>
       <Navbar />
-      {
-        loading ? (
-          <ViewCharacterList>
-            <Stack sx={{ color: 'grey.500' }} spacing={2} direction="row">
-              <CircularProgress color="secondary" />
-              <CircularProgress color="success" />
-              <CircularProgress color="inherit" />
-            </Stack>
-          </ViewCharacterList>
-        ) : (
-          <ViewCharacterList>
-            <CharacterList people={people} openModal={openModal} setOpenModal={setOpenModal} />
-          </ViewCharacterList>
-        )
-      }
+      {loading && <p>Estamos cargando, no desesperes...</p>}
+      <ViewCharacterList>
+        <CharacterList />
+      </ViewCharacterList>
     </>
   );
 };
